66542581aef2e97da43dc68f96b4b1b41ee2677d
oumatoulacen <oumatoulacen@gmail.com>
2024-01-04 15:15:22 +0100

    Pascal's Triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''define a pascxal triangle function'''
+
+def pascal_triangle(n):
+    result = [[1]]
+    for i in range(n - 1):
+        x = 0
+        arr = []
+        result[i].append(0)
+        for j in range(len(result[i])):
+            arr.append(x + result[i][j])
+            x = result[i][j]
+        result.append(arr)
+    result[-1].append(0)
+    return [res[:-1] for res in result]
