dc9eb2e449cd23d7354c09354d996f223deaa97a
oumatoulacen <oumatoulacen@gmail.com>
2024-02-01 05:16:49 +0100

    add files + check

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,6 +5,7 @@
 def validUTF8(data):
     '''determines if a given data set represents a valid UTF-8 encoding.'''
     for i in data:
-        if i > 128:
+        if i >= 128:
             return False
+        
     return True
