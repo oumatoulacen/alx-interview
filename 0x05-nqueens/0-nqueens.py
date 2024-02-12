32dcba0212a7c83ad88c4275a2e81b9554770747
oumatoulacen <oumatoulacen@gmail.com>
2024-02-12 01:13:07 +0100

    n queens problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,7 +3,16 @@
 import sys
 
 
+if len(sys.argv) < 2:
+    print('Usage: nqueens N')
+    exit(1)
+if not isinstance(int(sys.argv[1]), int):
+    print('N must be a number')
+    exit(1)
 N = int(sys.argv[1])
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
 class solution:
