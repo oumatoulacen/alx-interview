5941fc0610dd7da43bd57d73c3997eb255e4c8ce
oumatoulacen <oumatoulacen@gmail.com>
2024-02-16 03:58:35 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,27 @@
 #!/usr/bin/node
-
+/* Write a script that prints all characters of a Star Wars movie:
+    - The first argument is the movie ID
+    - You must use the Star wars API with the endpoint https://swapi-api.hbtn.io/api/films/:id
+    - You must use the module request
+*/
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.error('error:', error);
-    } else {
-        const results = JSON.parse(body).results;
-        for (let i = 0; i < results.length; i++) {
-        console.log(results[i].name);
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
         }
+      });
     }
-});
+  }
+})
+
