8e70d0f32384e42a207543d15f2a6c12c8fdf2c1
oumatoulacen <oumatoulacen@gmail.com>
2024-02-16 03:51:04 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/people/';
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.error('error:', error);
+    } else {
+        const results = JSON.parse(body).results;
+        for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        }
+    }
+});
