7ce0c88a7526df0badbc4d037eef4b03d5440e43
oumatoulacen <oumatoulacen@gmail.com>
2024-02-16 04:01:42 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,7 +6,7 @@
 */
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
