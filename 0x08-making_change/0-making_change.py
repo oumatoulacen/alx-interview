fd2abf2616f3662aa4f21d4ec93ccd75f671060e
Oumatou <oumatoulacen@gmail.com>
2024-05-28 23:54:35 +0100

    chore: Refactor makeChange function to adhere to PEP8 style guide

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' Making Change '''
 
+
 def makeChange(coins, total):
     ''' Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total
